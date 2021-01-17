c2897179b9cde3b449f9e51d56a8b68c39ea6028
Dynotum <sedano.carlos93@gmail.com>
2021-01-16 23:33:35 -0600

    add serversockets

--- Streams/src/Main.java
+++ Streams/src/Main.java
@@ -1,6 +1,7 @@
 import org.w3c.dom.html.HTMLParagraphElement;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -171,5 +172,41 @@ public class Main {
         }
         return sb.toString().toCharArray();
     }
+    public class EchoServer {
+        private static final int PORT = 34522;
 
+        public static void main(String[] args) {
+            try (ServerSocket server = new ServerSocket(PORT)) {
+                while (true) {
+                    Session session = new Session(server.accept());
+                    session.start(); // it does not block this thread
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class Session extends Thread {
+        private final Socket socket;
+
+        public Session(Socket socketForClient) {
+            this.socket = socketForClient;
+        }
+
+        public void run() {
+            try (
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream output = new DataOutputStream(socket.getOutputStream())
+            ) {
+                for (int i = 0; i < 5; i++) {
+                    String msg = input.readUTF();
+                    output.writeUTF(msg);
+                }
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
