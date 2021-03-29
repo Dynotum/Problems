28711c4a8453e0ffbdc46fa15d654e46af82bf52
Dynotum <sedano.carlos93@gmail.com>
2021-03-28 22:06:36 -0600

    Check If Two String Arrays are Equivalent

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -163,6 +163,21 @@ public class Main {
         return sum;
     }
 
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+        StringBuilder w1 = new StringBuilder();
+        StringBuilder w2 = new StringBuilder();
+
+        for (String str : word1) {
+            w1.append(str);
+        }
+        for (String str : word2) {
+            w2.append(str);
+        }
+
+        return w1.toString().equals(w2.toString());
+    }
+
     public static String tournamentWinner(ArrayList<ArrayList<String>> competitions,
                                           ArrayList<Integer> results) {
 
